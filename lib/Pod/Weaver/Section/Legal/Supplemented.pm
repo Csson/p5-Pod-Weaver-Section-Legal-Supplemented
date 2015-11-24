8c27746eda5b3813573ca58d33ae61ecc954b745
Erik Carlsson <info@code301.com>
2015-11-24 15:00:19 +0000

    Init

---
+++ lib/Pod/Weaver/Section/Legal/Supplemented.pm
@@ -0,0 +1,27 @@
+use strict;
+use warnings;
+
+package Pod::Weaver::Section::Legal::Supplemented;
+
+# VERSION
+# ABSTRACT: Short intro
+
+
+
+1;
+
+__END__
+
+=pod
+
+=head1 SYNOPSIS
+
+    use Pod::Weaver::Section::Legal::Supplemented;
+
+=head1 DESCRIPTION
+
+Pod::Weaver::Section::Legal::Supplemented is ...
+
+=head1 SEE ALSO
+
+=cut
